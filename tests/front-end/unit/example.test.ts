fa3895c4cbaecda970b1d524319fe82356fe9a0b
Brianna Cerkiewicz <briannac@shaw.ca>
2021-11-24 11:09:43 -0800

    create example test

---
+++ tests/front-end/unit/example.test.ts
@@ -0,0 +1,13 @@
+describe('example test', () => {
+
+    it("passes", () => {
+        const expected = true
+        expect(expected).toBe(true);
+      });
+
+      it("fails", () => {
+        const expected = true
+        expect(expected).toBe(false);
+      });
+    
+  }); 
